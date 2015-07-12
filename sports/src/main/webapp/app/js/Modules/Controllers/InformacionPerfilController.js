78fed1ee115ee35a196e1a1e135c17186c5ef458
Luis Esteban López <llopezr@ucenfotec.ac.cr>
2015-07-12 17:37:39 -0600

    FullCalendar render listo

--- sports/src/main/webapp/app/js/Modules/Controllers/InformacionPerfilController.js
+++ sports/src/main/webapp/app/js/Modules/Controllers/InformacionPerfilController.js
@@ -2,32 +2,50 @@
  * Created by JuanManuel on 09/07/2015.
  */
 var gridEstablecimientos = {};
-App.controller('EstablecimientosController', ['$scope','$http', '$stateParams','uiGridConstants', function($scope,$http, $stateParams,uiGridConstants) {
-    // no filter for inbox
-    var path = 'server/establecimientos.json';
+App.controller('EstablecimientosController', [ '$scope', '$http',
+		'$stateParams', 'uiGridConstants',
+		function($scope, $http, $stateParams, uiGridConstants) {
+			// no filter for inbox
+			var path = 'server/establecimientos.json';
 
-        $http.get(path).then(function (resp) {
-            $scope.Establecimientos = resp.data;
-        });
+			$http.get(path).then(function(resp) {
+				$scope.Establecimientos = resp.data;
+			});
 
+		} ]);
 
+App.controller('InformacionPerfilController', [ '$scope', '$http',
+		'$stateParams', '$state',
+		function($scope, $http, $stateParams, $state) {
+			var path = 'server/establecimientos.json';
+			$scope.init = function() {
+				$scope.mostrarCalendario = false;
+				console.log("entrarndo")
+				$http.get(path).then(function(resp) {
+					console.log("ressssp", resp)
+					var establecimientos = resp.data;
+					for (var i = 0; i < establecimientos.length; i++) {
+						if (establecimientos[i].id == $stateParams.mid) {
+							$scope.establecimiento = establecimientos[i];
+						}
+					}
+				});
+			}
 
-}]);
+			$scope.init();
 
-App.controller('InformacionPerfilController', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
-    var path = 'server/establecimientos.json';
-    $scope.init = function(){
-        console.log("entrarndo")
-        $http.get(path).then(function (resp) {
-            console.log("ressssp",resp)
-            var establecimientos = resp.data;
-            for (var i = 0; i < establecimientos.length; i++) {
-                if (establecimientos[i].id == $stateParams.mid){
-                    $scope.establecimiento = establecimientos[i];
-                }
-            }
-        });
-    }
+			$scope.mostrarReservaciones = function() {
+				$scope.mostrarCalendario = true;
+			}
+			
+			$scope.mostrarInformacion = function(){
+				$scope.mostrarCalendario = false;
+				$state.go("app.perfil.informacion");
+			}
+			
+			$scope.mostrarServicios = function(){
+				$scope.mostrarCalendario = false;
+				$state.go("app.perfil.servicios");
+			}
 
-    $scope.init();
-}]);
+		} ]);
